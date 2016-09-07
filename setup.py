f115930fd228928b247e80eeb03d86370d25fa59
xrotwang <xrotwang@googlemail.com>
2016-09-07 10:43:01 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='pycldf',
-    version="0.6.0",
+    version="0.6.1",
     description='A python library to read and write CLDF datasets',
     long_description=read("README.rst"),
     author='Robert Forkel',
