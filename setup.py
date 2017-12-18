57d4634aa1ee5837353a368048fe60723d806436
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-12-18 16:12:56 +0100

    add python_requires, tag python 3.5 and 3.6 support

--- setup.py
+++ setup.py
@@ -9,11 +9,23 @@ def read(fname):
 setup(
     name='pycldf',
     version='1.0.8',
-    description='A python library to read and write CLDF datasets',
-    long_description=read('README.rst'),
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
+    description='A python library to read and write CLDF datasets',
+    keywords='',
+    license='Apache 2.0',
     url='https://github.com/cldf/pycldf',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'cldf=pycldf.__main__:main',
+        ]
+    },
+    platforms='any',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=[
         'clldutils>=1.13.10',
         'csvw>=0.1',
@@ -25,35 +37,27 @@ setup(
     extras_require={
         'dev': ['flake8', 'wheel', 'twine'],
         'test': [
+            'mock',
             'pytest>=3.1',
             'pytest-mock',
-            'mock',
             'pytest-cov',
             'coverage>=4.2',
         ],
     },
-    license='Apache 2',
-    zip_safe=False,
-    keywords='',
-    platforms='any',
+    long_description=read('README.rst'),
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'cldf=pycldf.__main__:main',
-        ]
-    },
 )
