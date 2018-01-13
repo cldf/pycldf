3c67109e6ae858cdf5c97070edf1949b92f4ff81
xrotwang <xrotwang@googlemail.com>
2018-01-13 08:32:28 +0100

    bumped version number

---
+++ CHANGES.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Version 1.1.0 UNRELEASED
+
+
+## Version 1.0.10
+
+closes https://github.com/cldf/pycldf/issues/55
+
--- src/pycldf/__init__.py
+++ src/pycldf/__init__.py
@@ -4,4 +4,4 @@ from pycldf.sources import *
 from pycldf.terms import *
 
 # flake8: noqa
-__version__ = "1.0.9"
+__version__ = "1.0.10"
