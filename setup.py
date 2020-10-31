eca45c8f1326772088e97248a22bc5aa1af24899
xrotwang <xrotwang@googlemail.com>
2020-10-31 08:47:53 +0100

    release 1.17.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycldf',
-    version='1.16.1.dev0',
+    version='1.17.0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='A python library to read and write CLDF datasets',
