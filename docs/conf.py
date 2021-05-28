8dc3f68f87fe480b2cede2a7ad7804acfff52235
Robert Forkel <xrotwang@googlemail.com>
2021-05-28 12:43:00 +0200

    bump version for development

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Robert Forkel'
 author = 'Robert Forkel'
 
 # The full version, including alpha/beta/rc tags
-release = '1.21.2'
+release = '1.21.3.dev0'
 
 
 # -- General configuration ---------------------------------------------------
