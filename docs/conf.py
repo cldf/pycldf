de850772a72dbaa3350fd005b474cdd601278b1b
Robert Forkel <xrotwang@googlemail.com>
2022-05-23 10:48:06 +0200

    bump version for development

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Robert Forkel'
 author = 'Robert Forkel'
 
 # The full version, including alpha/beta/rc tags
-release = '1.26.1'
+release = '1.26.2.dev0'
 
 
 # -- General configuration ---------------------------------------------------
--- src/pycldf/__init__.py
+++ src/pycldf/__init__.py
@@ -4,4 +4,4 @@ from pycldf.sources import *
 from pycldf.terms import *
 
 # flake8: noqa
-__version__ = "1.26.1"
+__version__ = "1.26.2.dev0"
