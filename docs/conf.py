5c7fa7b8d76e5c00086ed806892910cca839bd1a
Robert Forkel <xrotwang@googlemail.com>
2022-11-22 09:37:47 +0100

    release 1.30.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Robert Forkel'
 author = 'Robert Forkel'
 
 # The full version, including alpha/beta/rc tags
-release = '1.29.1.dev0'
+release = '1.30.0'
 
 
 # -- General configuration ---------------------------------------------------
